6f5afdd91ab81d418a81227451c7a499290024b8
ilyas abdighni <udunig@hotmail.com>
2016-01-03 11:42:37 +0100

    user post method is initially done

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -11,6 +11,7 @@ class Api extends REST_Controller{
   {
       // Construct the parent class
       parent::__construct();
+      $this->load->helper("my_api");
   }
 
   function users_get() {
@@ -30,13 +31,21 @@ class Api extends REST_Controller{
 
   }
 
+  //create a new internalUser
   function user_put() {
     $this->load->library("form_validation");
-    $this->form_validation->set_data($this->put());
+    $data = remove_unknown_fields( $this->put(),             $this->form_validation->get_field_names("user_put"));
+    $this->form_validation->set_data($data);
+    //$this->form_validation->set_message("is_unique[internaluser.InternalUserEmail]", "This email is already exist!");
     if ($this->form_validation->run("user_put") != false) {
       $this->load->model("Model_internal_users");
-      $user = $this->put();
-      $internalUser_id = $this->Model_internal_users->insert($user);
+      // validate email address if it exists
+      $email_address_exist = $this->Model_internal_users->get_by(array("InternalUserEmail" => $this->put("InternalUserEmail")));
+      if ($email_address_exist) {
+        $this->response(array("status" => "failed", "message" => "fdsaf"));
+      }
+      // insert the put data to the database
+      $internalUser_id = $this->Model_internal_users->insert($data);
       if (!$internalUser_id) {
         $this->response( array("status" => "failed", "message" => "An ucexpected error uccred when inserting to the database."), REST_Controller::HTTP_INTERNAL_SERVER_ERROR );
       } else {
@@ -45,6 +54,43 @@ class Api extends REST_Controller{
     } else {
       $this->response( array("status" => "failed", "message" => $this->form_validation->get_errors_as_array()), REST_Controller::HTTP_BAD_REQUEST );
     }
+  }
+
+  function user_post() {
+    $user_id = $this->uri->segment(3);
+    $this->load->model("Model_internal_users");
+    // $users = array(
+    //   1 => array("first_name" => "constant_ilyas", "last_name" => "constant_abdighni"),
+    //   2 => array("first_name" => "constant_gvlmerem", "last_name" => "constant_mutellip"),
+    // );
+    $user = $this->Model_internal_users->get_by(array("idInternalUser" => $user_id ));
+    if (isset($user["idInternalUser"])) {
+        $this->load->library("form_validation");
+        $data = remove_unknown_fields( $this->post(),             $this->form_validation->get_field_names("user_post"));
+        $this->form_validation->set_data($data);
+        //$this->form_validation->set_message("is_unique[internaluser.InternalUserEmail]", "This email is already exist!");
+        if ($this->form_validation->run("user_post") != false) {
+          $this->load->model("Model_internal_users");
+          // validate email address if it exists
+          $safe_email_address = !isset($data["InternalUserEmail"]) || $data["InternalUserEmail"] == $user["InternalUserEmail"] || !$this->Model_internal_users->get_by(array("InternalUserEmail" => $data["InternalUserEmail"]));
+          if (!$safe_email_address) {
+            $this->response( array("status" => "failed", "message" => "This email address is already in use."), REST_Controller::HTTP_INTERNAL_SERVER_ERROR );
+          }
+          // insert the post data to the database
+          $updated = $this->Model_internal_users->update($user_id, $data);
+          if (!$updated) {
+            $this->response( array("status" => "failed", "message" => "An ucexpected error uccred when updating to the database."), REST_Controller::HTTP_INTERNAL_SERVER_ERROR );
+          } else {
+            $this->response(array("status" => "success", "message" => "successfully inserted to the database"), REST_Controller::HTTP_OK);
+          }
+        } else {
+          $this->response( array("status" => "failed", "message" => $this->form_validation->get_errors_as_array()), REST_Controller::HTTP_BAD_REQUEST );
+        }
+    } else {
+      $this->response(array("status" => "failed", "message" => "The specified user could not be found."), REST_Controller::HTTP_NOT_FOUND);
+    }
+
+
   }
 
 
--- application/models/model_internal_users.php
+++ application/models/model_internal_users.php
@@ -9,10 +9,19 @@
     protected $return_type = "array";
 
     protected $after_get = array("remove_sensitive_data");
+    protected $before_create = array("prep_data");
 
     protected function remove_sensitive_data($user) {
       unset($user["InternalUserPassword"]);
       unset($user["InternalUserConfirmPassword"]);
+      return $user;
+    }
+
+    protected function prep_data($user) {
+      $user["InternalUserPassword"] = md5($user["InternalUserPassword"]);
+      $user["InternalUserConfirmPassword"] = md5($user["InternalUserConfirmPassword"]);
+      //$user["ip_address"] = $this->input->ip_address();
+      //$user["created_at"] = date("Y-m-d: H:i:s");
       return $user;
     }
 
